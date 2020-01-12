351f6d9232067340fe15fcaba49535813ea36dae
alexanders <alexanders@playtika.com>
2020-01-12 16:48:24 +0300

    adding visit entity. update owner entity

--- pet-clinic-data/src/main/java/com/alexanders/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/alexanders/petclinic/model/Owner.java
@@ -1,6 +1,15 @@
 package com.alexanders.petclinic.model;
 
+import java.util.Set;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Owner extends Person {
+
+    private Set<Pet> pets;
 
     public Owner(String firstName, String lastName) {
         super(firstName, lastName);

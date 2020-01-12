df130c2c2b9719d5439f23006bd5a56aba92e29a
alexanders <alexanders@playtika.com>
2020-01-12 16:57:33 +0300

    adding speciality entity. update ver entity

---
+++ pet-clinic-data/src/main/java/com/alexanders/petclinic/model/Speciality.java
@@ -0,0 +1,11 @@
+package com.alexanders.petclinic.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Speciality extends BaseEntity {
+
+    private String description;
+}
--- pet-clinic-data/src/main/java/com/alexanders/petclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/alexanders/petclinic/model/Vet.java
@@ -1,6 +1,15 @@
 package com.alexanders.petclinic.model;
 
+import java.util.Set;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Vet extends Person {
+
+    private Set<Speciality> specialities;
 
     public Vet(String firstName, String lastName) {
         super(firstName, lastName);
